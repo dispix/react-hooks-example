e14d3ba11a59e21af562493efad0ca6f38c58cf5
Octave Raimbault <me@dispix.net>
2018-10-29 00:48:36 +0100

    Initial page

---
+++ build/precache-manifest.47776d785427577be6bb574e2e46d230.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c63c12a74fbdffb1935c",
+    "url": "/static/js/main.c63c12a7.chunk.js"
+  },
+  {
+    "revision": "3b45dff6b2030374c636",
+    "url": "/static/js/1.3b45dff6.chunk.js"
+  },
+  {
+    "revision": "c63c12a74fbdffb1935c",
+    "url": "/static/css/main.92697ba9.chunk.css"
+  },
+  {
+    "revision": "5d4b467b288e4f4213ef8c7766798973",
+    "url": "/index.html"
+  }
+];
